ce6c3979d677ee9c1ac7f727c7642a727b800f9f
Helena Cruz <helcruz8@gmail.com>
2016-12-30 01:40:13 +0000

    fixed indentation

--- tomobi.sh
+++ tomobi.sh
@@ -14,7 +14,7 @@ find $1 -type f -print0 | while read -d $'\0' file; do
         echo "Converting $file..."
         filename=${file%.*}
         ebook-convert "$file" "$filename.mobi"
-		echo "Done!"
+        echo "Done!"
     fi
 done
 
