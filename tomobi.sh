c6198863f74225159defa205fb2f29bb30f7025f
Helena Cruz <helcruz8@gmail.com>
2016-12-30 01:24:13 +0000

    converts from epub to mobi using calibre

---
+++ tomobi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "Missing input directory"	
+	exit 1
+else
+    echo $1
+fi
+
+find $1 -type f -print0 | while read -d $'\0' file; do
+	if [[ $file == *.epub ]]
+	then
+		echo "Trying to convert $file"
+		filename=${file%.*}
+		ebook-convert "$file" "$filename.mobi"
+	fi
+done
