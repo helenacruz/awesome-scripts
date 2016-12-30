711ed72006003872e01424c9f5ebad21aa657094
Helena Cruz <helcruz8@gmail.com>
2016-12-30 01:38:29 +0000

    changed prints

--- tomobi.sh
+++ tomobi.sh
@@ -11,8 +11,10 @@ fi
 find $1 -type f -print0 | while read -d $'\0' file; do
     if [[ $file == *.epub ]]
     then
-        echo "Trying to convert $file"
+        echo "Converting $file..."
         filename=${file%.*}
         ebook-convert "$file" "$filename.mobi"
+		echo "Done!"
     fi
 done
+
